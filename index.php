f81dc5aa35f20a3ddc00f04e2b331bc37ef7e9a8
devraj88 <devrajse1@gmail.com>
2017-07-19 14:56:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>moviesdb</title>
+<title>Omdb Coding Challenge</title>
 <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 <link href="css/animate.css" rel="stylesheet" type="text/css" />
 <script src="js/jquery.min.js" type="text/javascript" defer="defer"></script>
